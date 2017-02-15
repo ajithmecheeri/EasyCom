0e952693e2b9c2b2cae4eb10012365f19aebf442
ameche <JT74@global.tesco.org>
2017-02-15 10:04:12 +0530

    angular sample changes

---
+++ EasyShop/Controllers/HelpController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EasyShop.Controllers
+{
+    public class HelpController : Controller
+    {
+        // GET: ProductAngular1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ProductAngular1
+        public ActionResult BasicAngularSample()
+        {
+            return View();
+        }
+    }
+}
